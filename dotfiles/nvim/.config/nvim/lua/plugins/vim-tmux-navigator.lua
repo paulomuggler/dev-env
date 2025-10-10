c7f40a29d0892b040fa88d1ad9a4abe716d77dbc
Paulo Muggler <pmugglerm@gmail.com>
2025-10-11 01:48:11 +0200

    Disable alt+arrows pane navigation, leave only ctrl+hjkl (vim-tmux-navigator)

--- dotfiles/nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -3,10 +3,16 @@ return {
   "christoomey/vim-tmux-navigator",
   lazy = false, -- Load immediately for navigation to work properly
   keys = {
+    -- Ctrl+hjkl navigation (primary - works everywhere)
     { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate Left" },
     { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate Down" },
     { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate Up" },
     { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate Right" },
     { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Navigate Previous" },
   },
+  init = function()
+    -- Disable default plugin mappings, we'll set our own
+    vim.g.tmux_navigator_no_mappings = 0
+    vim.g.tmux_navigator_save_on_switch = 2
+  end,
 }
